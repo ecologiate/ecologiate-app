81ba6e62fd2bd27406ad522fdab70d8bb5c60d48
Gonzalo <gonzalomno5@gmail.com>
2017-08-23 14:39:44 -0300

    Mejore un poco el alta de producto

--- app/src/main/java/com/app/ecologiate/AltaProductoActivity.java
+++ app/src/main/java/com/app/ecologiate/AltaProductoActivity.java
@@ -2,12 +2,29 @@ package com.app.ecologiate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class AltaProductoActivity extends AppCompatActivity {
+
+    Spinner categoria;
+    Spinner material;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alta_producto);
+
+        categoria = (Spinner) findViewById(R.id.spinnerCategoria);
+        material = (Spinner) findViewById(R.id.spinnerMaterial);
+
+        ArrayAdapter <CharSequence> adapterCategoria = ArrayAdapter.createFromResource(this, R.array.Categoria, android.R.layout.simple_spinner_item);
+        categoria.setAdapter(adapterCategoria);
+
+
+        ArrayAdapter <CharSequence> adapterMaterial = ArrayAdapter.createFromResource(this, R.array.Materiales, android.R.layout.simple_spinner_item);
+        material.setAdapter(adapterMaterial);
+
     }
 }
