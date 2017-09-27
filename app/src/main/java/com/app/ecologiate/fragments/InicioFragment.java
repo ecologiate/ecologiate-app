245023e92d2985fd092b28f67e8d4c523b4b5cc5
Gonzalo <gonzalomno5@gmail.com>
2017-09-27 20:12:39 -0300

    Pantalla inicio 3.0

--- app/src/main/java/com/app/ecologiate/fragments/InicioFragment.java
+++ app/src/main/java/com/app/ecologiate/fragments/InicioFragment.java
@@ -4,12 +4,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.app.ecologiate.R;
+import com.app.ecologiate.models.Tip;
+import com.app.ecologiate.models.TipAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +25,9 @@ import butterknife.ButterKnife;
 
 public class InicioFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     private OnFragmentInteractionListener mListener;
 
     public InicioFragment() {}
@@ -37,6 +48,31 @@ public class InicioFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
         ButterKnife.bind(this, view);
 
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.tipFragment);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(view.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //creo mis mails
+        List<Tip> myDataset = new ArrayList<>();
+
+        myDataset.add(new Tip("Cumpleaños", "Utiliza los rollos de papel higiénico para hacer ingeniosas decoraciones ", R.raw.tip1));
+        myDataset.add(new Tip("Bolsas de Te", "Guarda tus bolsas de té usadas en el refrigerador y utilízalas a la mañana siguiente para ponerla sobre tus ojos y aliviar las bolsas debajo de ellos. ", R.raw.tip2));
+        myDataset.add(new Tip("Individuales", "Tus jeans también podrían transformarse en atractivos individuales para proteger tu mesa.", R.raw.tip3));
+        myDataset.add(new Tip("Neumaticos", "Puedes utilizar tus neumaticos para hacer tachos de basura.", R.raw.tip4));
+        myDataset.add(new Tip("Disquete", "Utiliza tus disquetes como decorativas macetas.", R.raw.tip5));
+
+        // specify an adapter (see also next example)
+        mAdapter = new TipAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
         //Acción del botón reciclar
         reciclar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,31 +84,13 @@ public class InicioFragment extends Fragment {
             }
         });
 
+
+
+
         return view;
     }
 
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
 
     public interface OnFragmentInteractionListener {
