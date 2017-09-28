af619ac6ee9834fecc58574ead14a5b574ac6d18
Gonzalo <gonzalomno5@gmail.com>
2017-09-27 21:59:47 -0300

    Pantalla inicio 4.0

--- app/src/main/java/com/app/ecologiate/fragments/InicioFragment.java
+++ app/src/main/java/com/app/ecologiate/fragments/InicioFragment.java
@@ -3,6 +3,7 @@ package com.app.ecologiate.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,10 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.app.ecologiate.R;
 import com.app.ecologiate.models.Tip;
 import com.app.ecologiate.models.TipAdapter;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,11 +33,22 @@ public class InicioFragment extends Fragment {
     private RecyclerView.LayoutManager mLayoutManager;
     private OnFragmentInteractionListener mListener;
 
+
+
     public InicioFragment() {}
 
     //Creo el botón.
-    @BindView(R.id.btnReciclar)
-    Button reciclar;
+    //@BindView(R.id.btnReciclar)
+    //Button reciclar;
+
+    @BindView(R.id.avatar)
+    ImageView imagenAvatar;
+
+    @BindView(R.id.md_floating_action_reciclar)
+    FloatingActionButton fam;
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,6 +61,7 @@ public class InicioFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_inicio, container, false);
         ButterKnife.bind(this, view);
+        imagenAvatar.requestFocus();
 
         mRecyclerView = (RecyclerView) view.findViewById(R.id.tipFragment);
 
@@ -65,26 +80,23 @@ public class InicioFragment extends Fragment {
 
         myDataset.add(new Tip("Cumpleaños", "Utiliza los rollos de papel higiénico para hacer ingeniosas decoraciones ", R.raw.tip1));
         myDataset.add(new Tip("Bolsas de Te", "Guarda tus bolsas de té usadas en el refrigerador y utilízalas a la mañana siguiente para ponerla sobre tus ojos y aliviar las bolsas debajo de ellos. ", R.raw.tip2));
-        myDataset.add(new Tip("Individuales", "Tus jeans también podrían transformarse en atractivos individuales para proteger tu mesa.", R.raw.tip3));
-        myDataset.add(new Tip("Neumaticos", "Puedes utilizar tus neumaticos para hacer tachos de basura.", R.raw.tip4));
-        myDataset.add(new Tip("Disquete", "Utiliza tus disquetes como decorativas macetas.", R.raw.tip5));
+
 
         // specify an adapter (see also next example)
         mAdapter = new TipAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
 
-        //Acción del botón reciclar
-        reciclar.setOnClickListener(new View.OnClickListener() {
+
+        fam.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Fragment reciclarFragment = new ReciclarFragment();
                 getActivity().getSupportFragmentManager().beginTransaction()
                         .replace(R.id.contentFragment, reciclarFragment)
                         .commit();
+
             }
         });
-
-
 
 
         return view;
