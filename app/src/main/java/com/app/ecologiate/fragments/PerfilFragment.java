3cbe7ca74c7fdcdfdbd0eadb2c623522175ba093
Diego <vidalpenadiego@gmail.com>
2017-10-19 01:57:09 -0300

    Perfil - Diálogo

--- app/src/main/java/com/app/ecologiate/fragments/PerfilFragment.java
+++ app/src/main/java/com/app/ecologiate/fragments/PerfilFragment.java
@@ -1,12 +1,18 @@
 package com.app.ecologiate.fragments;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.app.ecologiate.R;
 
@@ -47,8 +53,22 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        //DVP: Manejo el fragment como un view, creo el botón y en el onclick llamo
+        //al dialog.
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        ImageButton imagen = (ImageButton) view.findViewById(R.id.ibAceite);
+
+        imagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Medallas");
+                builder.setMessage("Oro, Plata o Bronce");
+                Dialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
