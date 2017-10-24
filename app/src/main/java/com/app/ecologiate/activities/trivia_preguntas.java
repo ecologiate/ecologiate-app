07e4bf53466566eb0263a2a8d388c9bb7b6a20d9
Diego <vidalpenadiego@gmail.com>
2017-10-24 01:12:51 -0300

    Pantallas de la trivia.

---
+++ app/src/main/java/com/app/ecologiate/activities/trivia_preguntas.java
@@ -0,0 +1,15 @@
+package com.app.ecologiate.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.ecologiate.R;
+
+public class trivia_preguntas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia_preguntas);
+    }
+}
