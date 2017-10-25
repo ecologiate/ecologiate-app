62980de162efb21aff63dafce16bc32fe6ee1cd4
Diego <vidalpenadiego@gmail.com>
2017-10-25 00:55:03 -0300

    Temas de la trivia

--- app/src/main/java/com/app/ecologiate/fragments/TriviaFragment.java
+++ app/src/main/java/com/app/ecologiate/fragments/TriviaFragment.java
@@ -1,14 +1,17 @@
 package com.app.ecologiate.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import com.app.ecologiate.R;
+import com.app.ecologiate.activities.trivia_preguntas;
 
 
 public class TriviaFragment extends AbstractEcologiateFragment {
@@ -24,8 +27,19 @@ public class TriviaFragment extends AbstractEcologiateFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trivia, container, false);
+        //DVP: Manejo el fragment como un view, creo el botón y en el onclick llamo
+        //a la función que invocará al servicio del back.
+        View view = inflater.inflate(R.layout.fragment_trivia, container, false);
+        Button botonEmpezarTrivia = (Button) view.findViewById(R.id.btnEmpezarTrivia);
+
+        botonEmpezarTrivia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), trivia_preguntas.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
